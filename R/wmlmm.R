1b6da072df1c3cc0a4109feae01eae320005ab97
joaomessa <42588062+joaomessa@users.noreply.github.com>
2020-10-22 22:28:17 -0300

    Update and rename veiga.R to wmlmm.R
    Fix one variable (only) fit error and addition of references.

--- R/wmlmm.R
+++ R/wmlmm.R
@@ -1,20 +1,32 @@
-# You can learn more about package authoring with RStudio at:
+#  Fit linear mixed-effects models incorporating sampling weights
 #
-#   http://r-pkgs.had.co.nz/
+#  Uses: probability-weighted IGLS (PWIGLS)
 
 #' Fit Weighted Multivariate Linear Multilevel Model to Longitudinal Data
 #'
 #' Fit a two-level probability-weighted multivariate linear model with a linear error covariance matrix structure, via IGLS algorithm.
+#'
+#' Follows estimation process described in Veiga et al. (2014). Uses probability-weighted IGLS with scaled weights.
 #'
 #' @param formula a linear formula object with the response on the left of a ~ operator and the terms, separated by + operators, on the right.
 #' @param data an optional data frame containing the variables in \code{formula}. If not found in data, the variables are taken from the environment of \code{formula} (if specified as a formula) or from the parent frame (if specified as a character vector).
 #' @param ID3 vector of indexes for level two units
 #' @param ID2 vector of indexes for level one units.
 #' @param ID1 vector of successive measurements within the same level one unit, for all units.
-#' @param wj a vector of sample weights for level two units. Level two units are selected with inclusion probabilities. Then, sample weights for the level two units are defined as the inverse of these probabilities.
-#' @param wij a vector of sample weights for level one units. After selecting a level two unit, level one units belonging to them are selected with inclusion probabilities. Then, sample weights for the level one units are defined as the inverse of these probabilities.
+#' @param wj a vector of sampling weights for level two units. Level two units are selected with inclusion probabilities. Then, sampling weights for the level two units are defined as the inverse of these probabilities.
+#' @param wij a vector of sampling weights for level one units. After selecting a level two unit, level one units belonging to them are selected with inclusion probabilities. Then, sampling weights for the level one units are defined as the inverse of these probabilities.
 #' @param type type of structure imposed in the error covariance matrix; "toep" refers to the toeplitz, "uns" refers to the unestructured and "genlin" refers to the general linear.
 #' @param rot vector of 0's and 1's related to the measurements in time when \code{"genlin"} is passed to the \code{type} argument. Use 1 if the data were collected in that specific time unit, and 0 otherwise.
+#'
+#' @references
+#' A. Veiga, P. W. F. Smith and J. J. Brown (2014),
+#' The use of sample weights in multivariate multilevel models with an application to income data collected by using a rotating panel survey
+#' Journal of the Royal Statistical Society. Series C (Applied Statistics)
+#' Vol. 63, No. 1 (JANUARY 2014), pp. 65-84 (20 pages)
+#'
+#' @examples
+#'  wmlmm ( Y ~ X1 + X2, data = exampledata, idd, wave, wj, wi_j, "toep")
+#'
 #' @return Estimated list of estimators
 #' @export
 wmlmm <- function(formula, data = NULL, ID3, ID2, ID1, wj, wij, type, rot = NULL){
@@ -33,7 +45,7 @@ wmlmm <- function(formula, data = NULL, ID3, ID2, ID1, wj, wij, type, rot = NULL
 
   mm=model.matrix(formula, cl)
 
-  x <- mm[,-1]
+  x <- mm[,-1, drop=FALSE]
   x_names <- colnames(x)
   y <- as.numeric(model.response(cl))
 
@@ -78,7 +90,7 @@ wmlmm <- function(formula, data = NULL, ID3, ID2, ID1, wj, wij, type, rot = NULL
   }
 
   s <- length(DELTA)
-  h_matrix = diag(s)[1,] #aqui é um vetor
+  h_matrix = diag(s)[1,]
 
   name1 <- matrix(paste (deparse(substitute(ID1)), 1:tt, sep=" "))
   name1 <- rbind(name1, as.matrix(x_names))
@@ -123,14 +135,14 @@ wmlmm <- function(formula, data = NULL, ID3, ID2, ID1, wj, wij, type, rot = NULL
   teta0[2]=i_t$wj_t6/i_t$wj_aux
 
   #--------------------------------------------------------------------------
-  #                     IGLS - ITERATIVO
+  #                     IGLS - ITERATIVE
   #--------------------------------------------------------------------------
 
   itera = 1
 
   ss=s*s
 
-  ## Truques
+  ## Trick
   beta_ant = beta0
   beta = beta_ant*2
   teta_ant=teta0
@@ -192,7 +204,7 @@ wmlmm <- function(formula, data = NULL, ID3, ID2, ID1, wj, wij, type, rot = NULL
     r_mat = matrix(matr,s)
     s_mat = matrix(mats,s)
 
-    #-----teta = u0j , eij----#
+    #-----theta = u0j , eij----#
 
     if (itera != 1) {
       teta_ant = teta
@@ -209,7 +221,7 @@ wmlmm <- function(formula, data = NULL, ID3, ID2, ID1, wj, wij, type, rot = NULL
   teta_matrix <- eval(teta_loop)
 
   #-------------------------------------------------------------------------
-  # Vari?ncias
+  # Variances
   #-------------------------------------------------------------------------
   teta1 <- teta[1]
   teta_1 <- solve(teta1)
@@ -234,26 +246,27 @@ wmlmm <- function(formula, data = NULL, ID3, ID2, ID1, wj, wij, type, rot = NULL
     n,
     teta1)
 
-  dp_u <- sqrt(as.numeric(variances_resids$var_u))
+  dp_u <- sqrt(variances_resids$var_u)
 
-  #----------Variances-----------------
   var_beta = solve_s_matp%*%((m/(m-1))*variances_resids$s_mat_c)%*%solve_s_matp
   dp_beta = sqrt(diag(var_beta))
 
-  #var_teta = 2*solve(r_mat)
+  #var_teta = 2*solve(r_mat) in other context
   var_teta = solve_r_mat%*%(m/(m-1)*variances_resids$s_mat_d)%*%solve_r_mat
   dp_teta = sqrt(diag(var_teta))
 
   Sigma_r=teta1*matrix(1,tt,tt)+ teta_matrix
 
+  names(variances_resids$u) <- names(dp_u) <- as.vector(unique(clusters))
+  #names(variances_resids$v) <- names(variances_resids$yhat) <- rownames(mm) necessary? 6x heavier
   names(beta) <- name1
   names(teta) <- name3
   list_genlin <-list(beta = list(coefficients = beta, standard_errors = dp_beta, initial_values = beta0),
                      theta = list(coefficients = teta, standard_errors = dp_teta, initial_values = teta0),
                      mix_teta_matrix = teta_matrix, tot_var_matrix = Sigma_r, num_obs_1 = n_nivel_1, num_obs_2 = m,
-                     num_time_obs = tt, call = clprint, fitted_values = as.numeric(variances_resids$yhat),
-                     individual_residuals = as.numeric(variances_resids$v),
-                     group_residuals = list(coefficients = as.numeric(variances_resids$u), standard_errors = dp_u),
+                     num_time_obs = tt, call = clprint, fitted_values = variances_resids$yhat,
+                     individual_residuals = variances_resids$v,
+                     group_residuals = list(coefficients = variances_resids$u, standard_errors = dp_u),
                      iterations = itera-1
   )
   class(list_genlin) <- 'multivariate'
