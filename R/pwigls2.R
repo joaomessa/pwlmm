8e954f80e7d4438a668dc621c1e57b14ea9e80ee
joaomessa <42588062+joaomessa@users.noreply.github.com>
2020-10-22 22:22:53 -0300

    Update and rename pfeff.R to pwigls2
    Small changes like addition of references and functions to call residuals and fitted values

--- R/pwigls2.R
+++ R/pwigls2.R
@@ -1,6 +1,6 @@
-# You can learn more about package authoring with RStudio at:
+#   Fit linear mixed-effects models incorporating sampling weights
 #
-#   http://r-pkgs.had.co.nz/
+#   Uses: probability-weighted IGLS (PWIGLS)
 
 print.univariate <- function(m){
   cat(noquote("Call:\n"))
@@ -20,7 +20,6 @@ print.univariate <- function(m){
   cat(noquote("Variance components:\n"))
   result_theta = data.frame(estimate = c(m$theta$level2_variances,m$theta$level1_variance) ,
                             se = c(m$theta$se_level2_variances, m$theta$se_level1_variance))
-  #result_theta = rbind(result_theta, c(m$theta$level1_variance,44))
   names(result_theta) <- c("Estimate","Std. Error")
   print(result_theta, digits = 4)
 
@@ -41,14 +40,34 @@ print.univariate <- function(m){
   cat(noquote("Note: robust standard errors"))
 }
 
+residuals.univariate <- function(m){
+  m$individual_residuals
+}
+
+fitted.univariate <- function(m){
+  m$fitted_values
+}
+
 #' Fit Weighted Linear Multilevel Model
 #'
 #' Fit a probability-weighted two-level linear model with unequal selection probabilities at each level, via IGLS algorithm.
 #'
+#' Follows estimation process described in Pfeffermann et al. (1998). Uses probability-weighted IGLS with scaled weights.
+#'
 #' @param formula a two-sided linear formula object describing both the fixed-effects and random-effects part of the model, with the response on the left of a ~ operator and the terms, separated by + operators, on the right. Random-effects terms are distinguished by vertical bars (|) separating expressions for design matrices from grouping factors.
 #' @param data an optional data frame containing the variables in \code{formula}. If not found in data, the variables are taken from the environment of \code{formula} (if specified as a formula) or from the parent frame (if specified as a character vector).
-#' @param wj a vector of sample weights for level two units. Level two units are selected with inclusion probabilities. Then, sample weights for the level two units are defined as the inverse of these probabilities.
-#' @param wij a vector of sample weights for level one units. After selecting a level two unit, level one units belonging to them are selected with inclusion probabilities. Then, sample weights for the level one units are defined as the inverse of these probabilities.
+#' @param wj a vector of sampling weights for level two units. Level two units are selected with inclusion probabilities. Then, sampling weights for the level two units are defined as the inverse of these probabilities.
+#' @param wij a vector of sampling weights for level one units. After selecting a level two unit, level one units belonging to them are selected with inclusion probabilities. Then, sampling weights for the level one units are defined as the inverse of these probabilities.
+#'
+#' @references
+#' D. Pfeffermann; C. J. Skinner; D. J. Holmes; H. Goldstein; J. Rasbash, 2008,
+#' Weighting for Unequal Selection Probabilities in Multilevel Models
+#' Journal of the Royal Statistical Society. Series B (Statistical Methodology),
+#' Vol. 60, No. 1. (1998), pp. 23-40.
+#'
+#' @examples
+#' pwigls2( Y ~ X1 + X2 + (1|PSU), data = exampledata, wj, wij)
+#'
 #' @return Estimated list of estimators
 #' @export
 pwigls2 <-function(formula, data = NULL, wj, wij){
@@ -92,9 +111,6 @@ pwigls2 <-function(formula, data = NULL, wj, wij){
 
   p <- ncol(x)
 
-  #((I(s)[vec(makesymmetric(invvech(1::s-1))), ]))'
-  #name3 <- matrix(cbind("sigma2_u0","sigma_u10","sigma2_u1" ,"sigma2_e"))
-  #name3 <- matrix( paste("teta_", 0:(s-1), sep=""))
   name3 <- matrix(paste ("sigmau_", rep((0:(q-1)), (q:1)), ((sequence(q:1))-1)+ (rep((0:(q-1)), (q:1))), sep=""))
   name3<-rbind(name3, "sigma2_e")
 
@@ -138,12 +154,12 @@ pwigls2 <-function(formula, data = NULL, wj, wij){
   teta0 <- c(sit[lower.tri(sit, diag = T)], i_t$wj_t6/i_t$wj_aux)
 
   #--------------------------------------------------------------------------
-  #                     IGLS - ITERATIVO
+  #                     IGLS - ITERATIVE
   #--------------------------------------------------------------------------
 
   itera = 0
 
-  ## Truques
+  ## Trick
   beta_ant = beta0
   beta = beta_ant*2
   teta_ant=teta0
@@ -177,8 +193,6 @@ pwigls2 <-function(formula, data = NULL, wj, wij){
     beta = solve_s_matp %*% objeto1$s_matq
     beta <- as.numeric(beta)
 
-    ########################################
-
     objeto2 <- iterative_uni_theta(s,
                                    p,
                                    m,
@@ -199,7 +213,7 @@ pwigls2 <-function(formula, data = NULL, wj, wij){
                                    TJS$TR_T5_HK,
                                    TJS$T5_HK)
 
-    #-----teta ----
+    #-----theta ----
 
     if (itera != 0) {
       teta_ant = teta
@@ -209,7 +223,7 @@ pwigls2 <-function(formula, data = NULL, wj, wij){
     teta = solve_r_mat %*% objeto2$s_mat
     teta <- as.numeric(teta)
 
-    ie_teta <- invvech_eigen(teta[one_to_sminus1]) #Matriz de variância covariância sigmau
+    ie_teta <- invvech_eigen(teta[one_to_sminus1]) #Variance covariance matrix sigma_u
     sit <- solve(ie_teta)
 
     #------End of iterative process----------
@@ -223,7 +237,7 @@ pwigls2 <-function(formula, data = NULL, wj, wij){
   #-------------------------------------------------------------------------
 
   #-------------------------------------------------------------------------
-  # Vari?ncias
+  # Variances
   #-------------------------------------------------------------------------
 
   variances_residuals <- uni_variances_residuals(
@@ -249,27 +263,12 @@ pwigls2 <-function(formula, data = NULL, wj, wij){
     TJS$TR_T5_HK,
     TJS$T5_HK)
 
-  #----------Variances-----------------
   var_beta = solve_s_matp%*%((m /(m-1))*variances_residuals$s_matc)%*%solve_s_matp
   dp_beta = sqrt(diag(var_beta))
 
-  #var_teta = 2*solve(r_mat)
+  #var_teta = 2*solve(r_mat) in other context
   var_teta = solve_r_mat%*%(m/(m-1)*variances_residuals$s_matd)%*% solve_r_mat
   dp_teta = sqrt(diag(var_teta))
-
-  # z_star = beta/dp_beta
-  # z_star2= teta/dp_teta
-  #
-  # z_star_l= beta-abs(qnorm(0.025))*dp_beta
-  # z_star_u= beta+abs(qnorm(0.025))*dp_beta
-  # pz_star= 2*(1-pnorm(abs(z_star)))
-  #
-  # z_star2_l= teta-abs(qnorm(0.05))*dp_teta
-  # z_star2_u= teta+abs(qnorm(0.05))*dp_teta
-  # pz_star2= 2*(1-pnorm(abs(z_star2)))
-
-  #-------------------------------------------------------------------------
-
 
   #/*-------------------------------------------------------------------------
   #  Residuals
@@ -278,25 +277,11 @@ pwigls2 <-function(formula, data = NULL, wj, wij){
   if(any(variances_residuals$var_u<0))
     warning('Model failed to converge: negative residual(s) variance(s)')
 
-  #dp_v <- sqrt(variances_residuals$var_v) #Erros padrão dos resíduos de nível 1
-  dp_u <- sqrt(variances_residuals$var_u) #Erros padrão dos resíduos de nível 2
-
-  #u_pad = u / dp_u
-  #v_pad = v / dp_v
-  #-------------------------------------------------------------------
-  # N?vel 2: u dp_u u_pad
-  # N?vel 1: v dp_v v_pad
-  #-------------------------------------------------------------------
-
-  #residuosN2 <- data.frame(u, u_pad, dp_u)
-  #namesun2 <- c("u0", "u1", "upad0", "upad1", "dpu0", "dpu1")
-  #names(residuosN2) <- namesun2
-  #residuosN2$li <- residuosN2$u0-1.96*residuosN2$dpu0
-  #residuosN2$ls <- residuosN2$u0+1.96*residuosN2$dpu0
-  #residuosN2$idCluster <- cluster
+  dp_u <- sqrt(variances_residuals$var_u)
 
   colnames(variances_residuals$u) <- colnames(dp_u) <-paste("u", 0:(q-1), sep="")
-
+  rownames(variances_residuals$u) <- rownames(variances_residuals$var_u) <- as.vector(unique(clusters))
+  names(variances_residuals$v) <- names(variances_residuals$yhat) <- rownames(fr)
   names(teta) <- name3
 
   diag_matrix_invvech <- diag(matrix_invvech)
@@ -315,15 +300,6 @@ pwigls2 <-function(formula, data = NULL, wj, wij){
   } else {
     teta_cov <- se_cov <- NULL
   }
-
-  #residuosN1 <- data.frame(v, v_pad, sqrt(var_v))
-  #namesun1 <- c("e0", "epad0", "dpe0")
-  #names(residuosN1) <- namesun1
-
-  #residuosN2 <<- residuosN2
-  #residuosN1 <<- residuosN1
-
-  #finish= st_global("c(current_time)")
 
   names(beta) <- name1
   list_igls <-list(beta = list(coefficients = beta, standard_errors = dp_beta),
@@ -360,6 +336,14 @@ print.multivariate <- function(m){
 
   cat("\n")
   cat(noquote("Note: robust standard errors"))
+}
+
+residuals.multivariate <- function(m){
+  m$individual_residuals
+}
+
+fitted.multivariate <- function(m){
+  m$fitted_values
 }
 
 teta_structure_toep <- function(teta, s){
@@ -411,7 +395,7 @@ delta_gen <- function(rot, s){
               name_type = matrix(paste("Genlin ", 1:(length(DELTA)-1), sep=""))))
 }
 
-#Existe um jeito mais simples sem estrutura for
+#There's a more effective way which doesn't use 'for'
 delta_toep <- function(tt){
   k=0
 lag_size=tt-1
